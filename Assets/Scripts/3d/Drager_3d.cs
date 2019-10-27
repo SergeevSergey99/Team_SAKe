297f92cf7888b959d738823907ec63101f691e00
Sergey <Sergeev_sergey99@mail.ru>
2019-10-28 02:45:18 +0300

    Add 3d

---
+++ Assets/Scripts/3d/Drager_3d.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class Drager_3d : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    // Use this for initialization
+    private void Start()
+    {
+        zeroPoint = gameObject.GetComponent<RectTransform>().anchoredPosition;
+        if (!unit.CompareTag("Actor"))
+        {
+            cost = 10;
+            gameObject.transform.GetChild(0).GetComponent<Text>().text = cost.ToString();
+            return;
+        }
+        cost = unit.GetComponent<Unit1_3d>().cost;
+        gameObject.transform.GetChild(0).GetComponent<Text>().text = cost.ToString();
+    }
+
+    public GameObject resourses;
+    private Vector3 zeroPoint;
+    private int cost = 0;
+    public GameObject unit;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+//        float k = Screen.currentResolution.height / 733.0f;
+        float k = 1.0f;
+        //наводим курором на место где должен появиться юнит
+        gameObject.GetComponent<RectTransform>().anchoredPosition =
+            new Vector3(0, (Input.mousePosition.y) * k, 0);
+    }
+
+    private Random rand = new Random();
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        int res = Int32.Parse(resourses.GetComponent<Text>().text);
+        if (res >= cost && Time.timeScale > 0)
+        {
+            //создаём юнит в координатах где отпустили мышь
+            if (!gameObject.name.Equals("AvaMe"))
+            {
+                Instantiate(unit, new Vector3(9, 0.0f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y),
+                    Quaternion.Euler(45, 0, 0));
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    
+                    Instantiate(unit, new Vector3( rand.Next(-500, 500)/100.0f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y + 35 + rand.Next(-500, 500)/100.0f, rand.Next(-500, 500)/100.0f),
+                            Quaternion.Euler(45, 0, -90)).GetComponent<Rigidbody>().velocity = Vector3.down * 15;
+
+                }
+            }
+
+            res -= cost;
+            resourses.GetComponent<Text>().text = res.ToString();
+        }
+
+        gameObject.GetComponent<RectTransform>().anchoredPosition = zeroPoint;
+    }
+}
---
+++ Assets/Scripts/3d/SpawnMashine_3d.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class SpawnMashine_3d : MonoBehaviour
+{
+
+	private int resourse = 10;
+//	private float maxDistance = 8;
+	private Vector3 moveVector;
+	public GameObject unitB;
+	public GameObject unitZ;
+	public GameObject unitK;
+	public GameObject unitL;
+	// Use this for initialization
+	private Random rnd = new Random();
+	void Start ()
+	{
+		moveVector = gameObject.GetComponent<Unit1_3d>().GetMVector();
+		maxValue = Mathf.Max(Mathf.Max(unitB.GetComponent<Unit1_3d>().cost, unitL.GetComponent<Unit1_3d>().cost),
+			Mathf.Max(unitZ.GetComponent<Unit1_3d>().cost, unitK.GetComponent<Unit1_3d>().cost));
+	}
+
+	public int reloadMax = 60;
+	private int reload = 60;
+	private int resReload = 100;
+	private int maxValue;
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+
+		resReload--;
+		if (resReload <= 0)
+		{
+			resourse++;
+			resReload = 100;
+		}
+		
+	if (reload <= 0)
+	{
+		if (resourse >= maxValue)
+		{
+			Collider[] clds = Physics.OverlapSphere(gameObject.transform.position + moveVector * 4.0f, 3.0f);
+			int count = 0;
+			foreach (var colider in clds)
+			{
+				if (colider.gameObject.CompareTag("Actor") && colider.gameObject.GetComponent<Unit1_3d>().isOurTeam^gameObject.GetComponent<Unit1_3d>().isOurTeam)
+				{
+					count++;
+				}
+			}
+			int spawnZ;
+			int unitType;
+
+			if (count <= 0)
+			{
+				unitType = rnd.Next(1, 5);
+				spawnZ = rnd.Next(-4, 5);
+				reloadMax = 60;
+			}
+			else
+			{
+				reloadMax = 10;
+				resourse = maxValue;
+				unitType = rnd.Next(2, 4);
+				spawnZ = 0;
+			}
+			
+			switch (unitType)
+			{
+				case 1:
+					{
+						Instantiate(unitK, new Vector3(transform.position.x, 0, spawnZ), Quaternion.Euler(45, 0, 0));
+						reload = reloadMax;
+						resourse -= unitK.GetComponent<Unit1_3d>().cost;
+					}
+
+					break;
+				case 2:
+					{
+						Instantiate(unitB, new Vector3(transform.position.x, 0, spawnZ), Quaternion.Euler(45, 0, 0));
+						reload = reloadMax;
+						resourse -= unitB.GetComponent<Unit1_3d>().cost;
+					}
+
+					break;
+				case 3:
+					{
+						Instantiate(unitZ, new Vector3(transform.position.x, 0, spawnZ), Quaternion.Euler(45, 0, 0));
+						reload = reloadMax;
+						resourse -= unitZ.GetComponent<Unit1_3d>().cost;
+					}
+
+					break;
+				case 4:
+					{
+						Instantiate(unitL, new Vector3(transform.position.x, 0, spawnZ), Quaternion.Euler(45, 0, 0));
+						reload = reloadMax;
+						resourse -= unitL.GetComponent<Unit1_3d>().cost;
+					}
+
+					break;
+			}
+		}
+	}
+	else
+		{
+			reload--;
+		}
+	}
+}
