6ff87b2f518e02b7b45587ebe3274ddcab10fc14
Sergey <Sergeev_sergey99@mail.ru>
2019-11-16 01:34:16 +0300

    Wolf and Eggs base scripts

---
+++ Junk/WolfnEggs/counter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class counter : MonoBehaviour
+{
+    public GameObject text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private int cnt = 0;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        cnt++;
+        text.GetComponent<Text>().text = cnt.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Junk/WolfnEggs/instatiator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instatiator : MonoBehaviour
+{
+    
+    
+    public GameObject go;
+    public int timer = 1000;
+
+    public int cnt = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer--;
+        if (timer <= 0)
+        {
+            cnt--;
+            timer = 500;
+            Instantiate(go, 
+                new Vector3(transform.position.x + Random.Range(-5.0f, 5.0f), transform.position.y, 
+                    transform.position.z + Random.Range(-5.0f, 5.0f)),Quaternion.identity);
+            
+        }
+
+        if (cnt <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
