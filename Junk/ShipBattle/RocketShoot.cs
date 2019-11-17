3f5828bcbb4e0a1151791383edee76912a67854a
Sergey <Sergeev_sergey99@mail.ru>
2019-11-17 07:20:24 +0300

    Add Ship Battle miniGame

---
+++ Junk/ShipBattle/RocketShoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RocketShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public GameObject rocket;
+    public float Speed = 10;
+    public void Fire()
+    {
+        var go = GameObject.Find("AR Camera");
+        Vector3 pos = new Vector3(go.transform.position.x,
+            go.transform.position.y - 1,
+            go.transform.position.z);
+        var torp = Instantiate(rocket, pos, Quaternion.Euler(go.transform.rotation.x + 90,
+                                                    go.transform.rotation.y,
+                                                    go.transform.rotation.z)
+            );
+        torp.transform.rotation = go.transform.rotation;    
+        torp.GetComponent<Rigidbody>().velocity = GameObject.Find("AR Camera").transform.forward * Speed;
+    }
+
+
+}
---
+++ Junk/ShipBattle/torpedo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class torpedo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject ps;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        var go = Instantiate(ps);
+        go.transform.position = transform.position;
+        go.GetComponent<ParticleSystem>().Play();
+
+        if (other.gameObject.CompareTag("Ship"))
+        {
+            var score = GameObject.Find("Score");
+            int tmp = Int32.Parse(score.GetComponent<Text>().text);
+            tmp++;
+            score.GetComponent<Text>().text = tmp.ToString();
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+    }
+
+    private int time = 1000;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time--;
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
